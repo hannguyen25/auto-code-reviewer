1ca42d43e1e60f33a958712ebe6aecad5842ca66
Han <han0961765543@gmail.com>
2026-08-23 00:54:11 +0700

    fix: second fast commit

--- test-file.ts
+++ test-file.ts
@@ -2,3 +2,4 @@ console.log('first commit for testing debounce');
 export const testFn = () => 'initial';
 console.log('amend to cancel');
 console.log('first fast commit');
+console.log('second fast commit');
